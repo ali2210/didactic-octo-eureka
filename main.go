2ceebd555d47c854379501aa2b35f936122a78eb
Ali Hassan <Alideveloper95@gmail.com>
2021-10-16 18:09:50 +0500

    lazo form update

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import(
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func main() {
+	gin.ForceConsoleColor()
+	default_router := gin.Default()
+	default_router.LoadHTMLGlob("templates/*")
+	default_router.GET("/", func(c *gin.Context){
+		c.HTML(http.StatusOK, "index.html", gin.H{})
+	})
+	default_router.GET("/dockerfile/create", func(c *gin.Context){
+		c.HTML(http.StatusOK, "lazo.html", gin.H{})
+	})
+	default_router.POST("/dockerfile/create", func(c *gin.Context){
+		c.HTML(http.StatusOK, "lazo.html", gin.H{})
+	})		
+	default_router.Run(":8080")
+}
